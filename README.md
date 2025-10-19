9bfcd1e411c0c7875e4f8bcb259a7eea2511027f
tn-loc <pptloc28@gmail.com>
2025-10-19 20:26:13 +0700

    Complete
    I did it, 10/10, would rcmed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Etch-a-Sketch
+Self-reminder:
+Check aspect-ratio;
+Play around with flexbox a little bit more;
--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const cont = document.querySelector(".container");
-for (let i=0; i<= 16; i++){
-    const board = document.createElement('div');
-    board.className = "grid";
-    cont.appendChild(board);
+const btn = document.querySelector(".btn")
+
+const getGrid = (num) => {
+    let count = 1
+    for ( let i = 0; i < num; i++){
+        const row = document.createElement('div');
+        row.className ="row"
+        for ( let j = 0; j < num; j++){
+            const cell = document.createElement('div');
+            cell.className = "cell";
+            row.appendChild(cell);
+            cell.textContent = count;
+            count++;
+            cell.addEventListener("mouseenter", () => {
+                cell.style.backgroundColor = "#3B9797";
+            })
+        }
+        cont.appendChild(row);
+    }
 }
+
+btn.addEventListener("click",() => {
+cont.innerHTML = "";
+const number = prompt("Enter your number:");
+getGrid(number);
+});
